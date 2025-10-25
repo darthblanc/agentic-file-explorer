c7d7432e66dd4f3ab8a4f93e7eefab8c82ce7258
Clement-Andi Edet <cedet@udallas.edu>
2025-10-25 00:26:48 -0500

    implemented agent flow added argument parser for cli loading DATA_DIR environmental variable at runtime, this variable is different depending on whether unittests or the actual flow are being run gave agent access to tools

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from agentic-file-explorer!")
+import os
+os.environ.setdefault("DATA_DIR", "data")
 
+from langchain_ollama import ChatOllama
+from langchain.agents import create_agent
+from basic_file_functions import tools
+from arguments import parser
+from logger import logger
+
+def main(args):
+    model_name = args.model
+    keep_logs = args.verbose
+    username = args.username
+    temperature = args.temperature
+
+    model = ChatOllama(model=model_name, temperature=temperature)
+    agent = create_agent(
+        model=model,
+        tools=tools
+    )
+
+    human_message: str = input(f"{username}: ")
+
+    while human_message:
+        response = agent.invoke({"messages":[{"role": "user", "content": human_message}]})
+        print("Agent:", response["messages"][-1].content)
+        logger(keep_logs=keep_logs, response=response)
+        human_message: str = input(f"{username}: ")
 
 if __name__ == "__main__":
-    main()
+    args = parser.parse_args()
+    main(args)
