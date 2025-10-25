2d0a594601487a7511a6eac1ebae94ee01d388f7
Clement-Andi Edet <cedet@udallas.edu>
2025-10-25 00:28:55 -0500

    functions that setup the working environment have been created

---
+++ setup_directory.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import os
+
+DATA_DIR = Path(os.environ["DATA_DIR"])
+
+def construct_file_path(path: str):
+   """Create a Path object for a file.\n
+      Create a directory called \"data\" if not already in existence.\n
+      This is where all operations will be observed.\n
+      File Path => data/{path}.
+   """
+
+   working_directory = DATA_DIR
+   working_directory.mkdir(exist_ok=True)
+
+   return DATA_DIR / path
+
+def construct_directory_path(path: str) -> Path:
+   """Create a Path object for a directory.\n
+      Create a directory called \"data\" if not already in existence.\n
+      This is where all operations will be observed.\n
+      Directory Path => data/{path}.
+   """
+
+   working_directory = DATA_DIR / path
+   working_directory.mkdir(exist_ok=True, parents=True)
+
+   return working_directory    
