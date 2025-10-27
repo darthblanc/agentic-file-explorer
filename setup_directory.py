7b19aa3154b726290b018dff24a55e1fd997b563
Clement-Andi Edet <cedet@udallas.edu>
2025-10-26 23:43:04 -0500

    created a function that has limited ability to access directories

--- setup_directory.py
+++ setup_directory.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import os
 
-DATA_DIR = Path(os.environ["DATA_DIR"])
+# DATA_DIR = Path(os.environ["DATA_DIR"])
+DATA_DIR = Path("data")
 
 def construct_file_path(path: str):
    """Create a Path object for a file.\n
@@ -26,3 +27,16 @@ def construct_directory_path(path: str) -> Path:
    working_directory.mkdir(exist_ok=True, parents=True)
 
    return working_directory    
+
+def construct_directory_path_limited(path: str) -> Path:
+   """Create a Path object for a directory.\n
+      Create a directory called \"data\" if not already in existence.\n
+      This is where all operations will be observed.\n
+      Directory Path => data/{path}.
+      Will not automatically create any subdirectories.
+   """
+
+   working_directory = DATA_DIR / path
+   working_directory.mkdir(exist_ok=True)
+
+   return working_directory  
