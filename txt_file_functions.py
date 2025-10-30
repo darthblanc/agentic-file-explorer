6d0270ab1f496040fe8b7377055bb53f421e07dc
Clement-Andi Edet <cedet@udallas.edu>
2025-10-30 14:20:50 -0500

    rebrand from basic_file_functions to txt_file_functions

---
+++ txt_file_functions.py
@@ -0,0 +1,62 @@
+from langchain.tools import tool
+from setup_directory import construct_file_path
+
+@tool(description="Read the contents of a txt file specified by a path.")
+def read_txt(path: str) -> str:
+    working_path = construct_file_path(path)
+
+    try:
+        with open(working_path, "r") as fd:
+            return fd.read()
+    except Exception as e:
+        return f"Error encountered while reading from file ({path}): {e}"
+    
+@tool(description="Read the contents of a txt file specified by a path as a list of lines.")
+def read_txt_lines(path: str) -> str:
+    working_path = construct_file_path(path)
+
+    try:
+        with open(working_path, "r") as fd:
+            return f"{fd.readlines()}"
+    except Exception as e:
+        return f"Error encountered while reading from file ({path}): {e}"
+
+@tool(description="Write new content to a txt file specified by a path. This tool will overwrite the previous content of the txt file.")
+def write_to_txt(path: str, content: str) -> str:
+    working_path = construct_file_path(path)
+
+    try:
+        with open(working_path, "w") as fd:
+            fd.write(content)
+            return f"Wrote: content to ({path})"
+    except Exception as e:
+        return f"Error encountered while writing to file ({path}): {e}"
+
+@tool(description="Write new content to a txt file specified by a path. This tool will append the new content to a new line after the previous content of the txt file.")
+def append_to_txt(path: str, content: str) -> str:
+    working_path = construct_file_path(path)
+
+    try:
+        with open(working_path, "a") as fd:
+            fd.write(f"\n{content}")
+            return f"Wrote: content to ({path})"
+    except Exception as e:
+        return f"Error encountered while writing to file ({path}): {e}"
+
+@tool(description="Clear the contents of a file specified by a path.")
+def clear(path: str) -> str:
+    working_path = construct_file_path(path)
+
+    try:
+        with open(working_path, "w") as fd:
+            fd.write("")
+            return f"Cleared: content in ({path})"
+    except Exception as e:
+        return f"Error encountered while writing to file ({path}): {e}"
+
+txt_tools = [
+    read_txt,
+    write_to_txt,
+    append_to_txt,
+    # clear,
+]
