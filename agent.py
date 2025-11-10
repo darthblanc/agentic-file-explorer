d19542c10d2bbbc22df3b8ae54cf6c2860e67ca1
Clement-Andi Edet <cedet@udallas.edu>
2025-11-09 22:16:29 -0600

    added system prompt

--- agent.py
+++ agent.py
@@ -5,12 +5,18 @@ from langchain_ollama import ChatOllama
 from langchain.agents import create_agent
 from agent_tools import tools
 
+system_prompt = ""
+
+with open("system_prompt.txt", "r") as fd:
+    system_prompt = fd.read()
+
 def my_agent(human_message: str, context=[]):
 
     model = ChatOllama(model="qwen3:8b", temperature=0)
     agent = create_agent(
         model=model,
-        tools=tools
+        tools=tools,
+        system_prompt=system_prompt
     )
 
     response = agent.invoke({"messages": context})
