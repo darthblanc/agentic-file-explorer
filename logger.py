adf60acc661f5a3407efe60b95d7d9c6912af081
Clement-Andi Edet <cedet@udallas.edu>
2025-11-15 00:53:09 -0600

    updated logger to allow specific files contains logs for different usages: CLI vs GUI

--- logger.py
+++ logger.py
@@ -1,8 +1,8 @@
 import logging
 from typing import Any
 
-def logger(keep_logs: bool, response: dict[str, Any] | Any):
+def logger(keep_logs: bool, response: dict[str, Any] | Any, filename = "agentic-fe.log"):
     if keep_logs:
-        logging.basicConfig(level=logging.INFO, filename="agentic-fe.log", format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.basicConfig(level=logging.INFO, filename=filename, format='%(asctime)s - %(levelname)s - %(message)s')
         tool_call_summary = "\n".join([f"\t\t{m.type} -> {m.content}" for m in response["messages"]])
         logging.info(f"\n\t\tSummary of conversation:\n{tool_call_summary}")
