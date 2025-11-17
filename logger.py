7037ec395e31509111bbbea9f0430884d98a85c1
Clement-Andi Edet <cedet@udallas.edu>
2025-11-16 22:15:43 -0600

    created context logger

--- logger.py
+++ logger.py
@@ -1,8 +1,16 @@
 import logging
-from typing import Any
+from typing import Any, List, Dict
 
-def logger(keep_logs: bool, response: dict[str, Any] | Any, filename = "agentic-fe.log"):
+def logger(keep_logs: bool, response: Dict[str, Any] | Any, filename = "agentic-fe.log"):
     if keep_logs:
         logging.basicConfig(level=logging.INFO, filename=filename, format='%(asctime)s - %(levelname)s - %(message)s')
-        tool_call_summary = "\n".join([f"\t\t{m.type} -> {m.content}" for m in response["messages"]])
-        logging.info(f"\n\t\tSummary of conversation:\n{tool_call_summary}")
+        tool_call_summary = "\n".join([f"{m.type} -> {m.content}" for m in response["messages"]])
+        logging.info(f"\n{tool_call_summary}")
+        logging.info("="*30)
+
+def ctx_logger(keep_logs: bool, context: List[Dict[str, str]] | Any, filename = "agentic-ctx.log"):
+    if keep_logs:
+        logging.basicConfig(level=logging.INFO, filename=filename, format='%(asctime)s - %(levelname)s - %(message)s')
+        context = "\n".join([ctx["content"] for ctx in context])
+        logging.info(f"\n{context}")
+        logging.info("="*30)
