24e04b7093fe06f124019810714e28aa3140f670
Clement-Andi Edet <cedet@udallas.edu>
2025-11-23 04:31:36 -0600

    now using a summarizer agent to manage context loading configs from stm config file created count_tokens function

--- context.py
+++ context.py
@@ -1,16 +1,7 @@
 from langchain_text_splitters import TokenTextSplitter
-from typing import Dict, List
-
-MAX_CONTEXT_WINDOW = 1000 # This might be about the effective context window for qwen3:8b
-system_prompt = ""
-with open("system_prompt.txt", "r") as fd:
-    system_prompt = fd.read()
-
-text_splitter = TokenTextSplitter.from_tiktoken_encoder(
-    chunk_size=1, chunk_overlap=0
-)
-
-system_prompt_size = len(text_splitter.split_text(system_prompt))
+from typing import Dict, List, Tuple
+from langchain.messages import HumanMessage, SystemMessage, AnyMessage
+from stm_loader import load_stm_config, load_summarization_agent
 
 def count_context_tokens(messages: List[Dict[str, str]]):
     text = ""
@@ -18,11 +9,29 @@ def count_context_tokens(messages: List[Dict[str, str]]):
         text += message_dict["content"] + "\n"
     return len(text_splitter.split_text(text))
 
-def trim_context(messages: List[Dict[str, str]]):
-    context_window = 0
-    for i, message_dict in enumerate(messages[::-1]):
-        context_window += len(text_splitter.split_text(message_dict["content"]))
-        if (system_prompt_size + context_window) >= MAX_CONTEXT_WINDOW:
-            return messages[-i-1:]
+def count_tokens(text: str):
+    return len(text_splitter.split_text(text))
 
-    return messages
+def trim_context(messages: List[AnyMessage], token_count: int) -> Tuple[bool, List[AnyMessage]]:
+    if token_count >= MAX_CONTEXT_WINDOW:
+        print("\n=======================INTERRUPTION START============================")
+        print("Trimming context...")
+        print(f"Current Context Size: {token_count}, Effective Context Size: {MAX_CONTEXT_WINDOW}")
+        response = ""
+        for token in agent.stream({"messages": messages + [HumanMessage(content="Summarise the following conversation focus on retaining information on goals, plans, knowledge.")]}, stream_mode="messages"): # type: ignore
+            if token[0].type == "AIMessageChunk": # type: ignore
+                response += token[0].content  # type: ignore
+        print(f"SUMMARY:\n{response}.\n")
+        print("Trimmed context.")
+        print("=======================INTERRUPTION END============================")
+        return True, [SystemMessage(content=response)]
+
+    return False, messages
+
+
+text_splitter = TokenTextSplitter.from_tiktoken_encoder(
+        chunk_size=1, chunk_overlap=0
+    )
+json_config = load_stm_config()
+MAX_CONTEXT_WINDOW = json_config["CONTEXT_SIZING"]["MAX_CONTEXT_WINDOW"]
+agent = load_summarization_agent(json_config["SUMMARIZATION_MODEL"])
