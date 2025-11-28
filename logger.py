060add7d7fcdadd99c576b4199a756dd34dad060
Clement-Andi Edet <cedet@udallas.edu>
2025-11-28 00:48:19 -0600

    updated logger to work with new message structure new message structure used langchain.messages

--- logger.py
+++ logger.py
@@ -4,7 +4,7 @@ from typing import Any, List, Dict
 def logger(keep_logs: bool, response: Dict[str, Any] | Any, filename = "agentic-fe.log"):
     if keep_logs:
         logging.basicConfig(level=logging.INFO, filename=filename, format='%(asctime)s - %(levelname)s - %(message)s')
-        tool_call_summary = "\n".join([f"{m.type} -> {m.content}" for m in response["messages"]])
+        tool_call_summary = "\n".join([f"{m["type"]} -> {m["content"]}" for m in response["messages"]])
         logging.info(f"\n{tool_call_summary}")
         logging.info("="*30)
 
