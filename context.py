20368b283192849965cf29eb4644841e75455d02
Clement-Andi Edet <cedet@udallas.edu>
2025-11-28 00:59:52 -0600

    created injected system prompts also importing configs

--- context.py
+++ context.py
@@ -1,7 +1,9 @@
 from langchain_text_splitters import TokenTextSplitter
 from typing import Dict, List, Tuple
-from langchain.messages import HumanMessage, SystemMessage, AnyMessage
-from stm_loader import load_stm_config, load_summarization_agent
+from langchain.messages import HumanMessage, SystemMessage, AIMessage, AnyMessage
+from stm_loader import load_summarization_agent
+from configs import CONTEXT_SIZING, SUMMARIZATION_MODEL, BASE_SYSTEM_PROMPT, REFERENCE_INSTRUCTIONS
+from file_dictionary import file_dict
 
 def count_context_tokens(messages: List[Dict[str, str]]):
     text = ""
@@ -14,24 +16,26 @@ def count_tokens(text: str):
 
 def trim_context(messages: List[AnyMessage], token_count: int) -> Tuple[bool, List[AnyMessage]]:
     if token_count >= MAX_CONTEXT_WINDOW:
-        print("\n=======================INTERRUPTION START============================")
+        print("\n\n=======================INTERRUPTION START============================")
         print("Trimming context...")
         print(f"Current Context Size: {token_count}, Effective Context Size: {MAX_CONTEXT_WINDOW}")
         response = ""
-        for token in agent.stream({"messages": messages + [HumanMessage(content="Summarise the following conversation focus on retaining information on goals, plans, knowledge.")]}, stream_mode="messages"): # type: ignore
+        for token in agent.stream({"messages": messages[:-1] + [HumanMessage(content="Summarise the following conversation focus on retaining information on goals, plans, knowledge. Ensure that you maintain the flow conversation.")]}, stream_mode="messages"): # type: ignore
             if token[0].type == "AIMessageChunk": # type: ignore
                 response += token[0].content  # type: ignore
-        print(f"SUMMARY:\n{response}.\n")
-        print("Trimmed context.")
-        print("=======================INTERRUPTION END============================")
-        return True, [SystemMessage(content=response)]
+        print(f"SUMMARY:\n{response}\n")
+        print("Trimmed context successfully.")
+        print("=======================INTERRUPTION END============================\n")
+        return True, [AIMessage(content=response), messages[-1]]
 
     return False, messages
 
+def inject_system_prompts(messages: List[AnyMessage]) -> List[AnyMessage]:
+    system_messages = [SystemMessage(content=BASE_SYSTEM_PROMPT), SystemMessage(content=REFERENCE_INSTRUCTIONS+"\n\nCurrent files in the system:\n"+str(file_dict)+"\n\n")]
+    return system_messages + messages
 
 text_splitter = TokenTextSplitter.from_tiktoken_encoder(
         chunk_size=1, chunk_overlap=0
     )
-json_config = load_stm_config()
-MAX_CONTEXT_WINDOW = json_config["CONTEXT_SIZING"]["MAX_CONTEXT_WINDOW"]
-agent = load_summarization_agent(json_config["SUMMARIZATION_MODEL"])
+MAX_CONTEXT_WINDOW = CONTEXT_SIZING["MAX_CONTEXT_WINDOW"]
+agent = load_summarization_agent(SUMMARIZATION_MODEL)
