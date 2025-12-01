b0610a4fa223e677a667fb0723fcd28c22159b8e
Clement-Andi Edet <cedet@udallas.edu>
2025-12-01 00:13:16 -0600

    stm now servers cli and gui

--- stm_context_agent.py
+++ stm_context_agent.py
@@ -3,20 +3,32 @@ from langchain.messages import AIMessage, HumanMessage
 from stm import ShortTermMemory, MessageContext
 from logger import logger
 
-def main(agent, chat_meta: chat_meta.ChatMeta):
+def main(agent, chat_meta: chat_meta.ChatMeta, user_prompt: str = ""):
     stm = ShortTermMemory(message_contexts=MessageContext(messages=[]), token_count=0)
+    
+    if not user_prompt:
+        while True:
+                user_prompt = input(f"{chat_meta.username}: ")
+                stm.add_message(HumanMessage(content=user_prompt))
 
-    while True:
-        user_prompt = input(f"{chat_meta.username}: ")
+                print(f"{chat_meta.model_name}: ", end="", flush=True)
+                response = ""
+                for token in agent.stream({"messages": stm.get_comprehensive_context()}, stream_mode="messages"): # type: ignore
+                    if token[0].type == "AIMessageChunk": # type: ignore
+                        print(token[0].content, end="", flush=True) # type: ignore
+                        response += token[0].content  # type: ignore
+                
+                print("\n\n")
+                stm.add_message(AIMessage(content=response))
+                logger(chat_meta.keep_logs, stm.get_message_contexts().model_dump(), filename="agentic.log")
+    else:
         stm.add_message(HumanMessage(content=user_prompt))
 
-        print(f"{chat_meta.model_name}: ", end="", flush=True)
         response = ""
         for token in agent.stream({"messages": stm.get_comprehensive_context()}, stream_mode="messages"): # type: ignore
             if token[0].type == "AIMessageChunk": # type: ignore
-                print(token[0].content, end="", flush=True) # type: ignore
                 response += token[0].content  # type: ignore
+                yield token[0].content  # type: ignore
         
-        print("\n\n")
         stm.add_message(AIMessage(content=response))
-        logger(chat_meta.keep_logs, stm.get_message_contexts().model_dump(), filename="agentic.log")
+        logger(chat_meta.keep_logs, stm.get_message_contexts().model_dump(), filename="agentic-ui.log")
