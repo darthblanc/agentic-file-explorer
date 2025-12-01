86bad9a01023bc536dad6462815029f56360089a
Clement-Andi Edet <cedet@udallas.edu>
2025-12-01 00:11:44 -0600

    updated agent interface with UI

--- agent.py
+++ agent.py
@@ -1,31 +1,26 @@
 import os
 os.environ.setdefault("DATA_DIR", "data")
 
+from chat_meta import ChatMeta
 from langchain_ollama import ChatOllama
 from langchain.agents import create_agent
 from agent_tools import tools
-from logger import logger, ctx_logger
-from context import trim_context
+from stm_context_agent import main as stm_main
+from configs import UI_CONFIGS
 
-system_prompt = ""
+chat_meta = ChatMeta(
+        model_name=UI_CONFIGS["model"]["default"],
+        keep_logs=UI_CONFIGS["verbose"]["default"],
+        username=UI_CONFIGS["username"]["default"],
+        temperature=UI_CONFIGS["temperature"]["default"],
+        stm=UI_CONFIGS["stm"]["default"])
 
-with open("system_prompt.txt", "r") as fd:
-    system_prompt = fd.read()
+model = ChatOllama(model=chat_meta.model_name, temperature=chat_meta.temperature, disable_streaming=False)
+agent = create_agent(
+    model=model,
+    tools=tools,
+)
 
-def my_agent(human_message: str, context=[]):
-
-    model = ChatOllama(model="qwen3:8b", temperature=0)
-    agent = create_agent(
-        model=model,
-        tools=tools,
-        system_prompt=system_prompt
-    )
-    trimmed_context = trim_context(context)
-    response = agent.invoke({"messages": trimmed_context}) # type: ignore
-    # logger(True, response, "agentic-ui.log")
-
-    response_content = response["messages"][-1].content
-    ctx_logger(True, trimmed_context + [{"role": "assistant", "content": response_content}])
-
-    new_context = {"messages": context + [{"role": "assistant", "content": response_content}]}
-    return response_content, new_context
+def agent_for_ui(user_prompt: str):
+    for token in stm_main(agent, chat_meta, user_prompt=user_prompt):
+        yield token
