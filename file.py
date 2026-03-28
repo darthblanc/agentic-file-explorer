8b22af82ca82e806287530bd733bf5e42033ae11
Clement-Andi Edet <cedet@udallas.edu>
2026-03-28 00:07:44 -0500

    to avoid memory errors removed holding file content in memory

--- file.py
+++ file.py
@@ -1,24 +1,26 @@
+import os
 from pydantic import BaseModel, Field
+from setup_directory import construct_file_path
 
 class File(BaseModel):
     id: str = Field(..., description="Reference ID of the file within the agent's context window (currently the path)")
     name: str = Field(..., description="Name of the file")
     path: str = Field(..., description="Path of the file")
-    content: str = Field(..., description="Content of the file")
     size: int = Field(..., description="Size of the file in bytes")
     content_type: str = Field(..., description="MIME type of the file")
 
     def get_content(self) -> str:
-        return self.content
+        with open(construct_file_path(self.path), "r") as f:
+            return f.read()
 
     @classmethod
-    def new_file(cls, path, content, content_type=""):
+    def new_file(cls, path, content_type=""):
+        working_path = construct_file_path(path)
         return cls(
             id=f"file:{path}",
             name=path.split("/")[-1],
             path=path,
-            content=content,
-            size=len(content.encode('utf-8')),
+            size=os.path.getsize(working_path),
             content_type=content_type
         )
 
