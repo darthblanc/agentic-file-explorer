8d87e0c77092b90178a6924cdd8a5c93d73ed5b3
Clement-Andi Edet <cedet@udallas.edu>
2026-03-27 23:56:01 -0500

    slight update to allow dynamic access to clear tool at runtime

--- agent_tools.py
+++ agent_tools.py
@@ -1,6 +1,6 @@
-from txt_tools import txt_tools
+from txt_tools import get_txt_tools
 from csv_tools import csv_tools
 from directory_tools import directory_tools
 from traversal_tools import traversal_tools
 
-tools = txt_tools + csv_tools + directory_tools + traversal_tools
+tools = get_txt_tools() + csv_tools + directory_tools + traversal_tools
