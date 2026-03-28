3d422171068d13408689301bf6bad9c6ad8bd468
Clement-Andi Edet <cedet@udallas.edu>
2026-03-27 23:56:54 -0500

    created session stm + cleaned up earlier bug with cli vs ui not working

--- agent.py
+++ agent.py
@@ -5,8 +5,10 @@ os.environ.setdefault("DATA_DIR", SANDBOX_DIR)
 from chat_meta import ChatMeta
 from langchain_ollama import ChatOllama
 from langchain.agents import create_agent
-from agent_tools import tools
-from stm_context_agent import main as stm_main
+from hierarchical_agent_tools import create_hierarchical_tools
+from stm_context_agent import stream as stm_stream
+from stm import ShortTermMemory, MessageContext
+from langchain_core.messages import HumanMessage
 from configs import UI_CONFIGS
 
 chat_meta = ChatMeta(
@@ -16,12 +18,21 @@ chat_meta = ChatMeta(
         temperature=UI_CONFIGS["temperature"]["default"],
         stm=UI_CONFIGS["stm"]["default"])
 
-model = ChatOllama(model=chat_meta.model_name, temperature=chat_meta.temperature, disable_streaming=False)
+model = ChatOllama(model=chat_meta.model_name, temperature=chat_meta.temperature, think=UI_CONFIGS["think"]["default"], disable_streaming=False)
 agent = create_agent(
     model=model,
-    tools=tools,
+    tools=create_hierarchical_tools(model, allow_clear_txt=UI_CONFIGS["allow_clear_txt"]["default"]),
 )
 
+session_stm = ShortTermMemory(message_contexts=MessageContext(messages=[]), token_count=0)
+
 def agent_for_ui(user_prompt: str):
-    for token in stm_main(agent, chat_meta, user_prompt=user_prompt):
+    for token in stm_stream(agent, chat_meta, user_prompt=user_prompt, stm=session_stm):
         yield token
+
+def reset_session_stm():
+    global session_stm
+    session_stm = ShortTermMemory(message_contexts=MessageContext(messages=[]), token_count=0)
+
+def inject_notice(content: str):
+    session_stm.add_message(HumanMessage(content=content))
