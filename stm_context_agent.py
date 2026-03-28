58c4d515a0125ff1c88cd34539ced40a40ed5e4a
Clement-Andi Edet <cedet@udallas.edu>
2026-03-28 00:12:54 -0500

    allowing agent to stream for ui

--- stm_context_agent.py
+++ stm_context_agent.py
@@ -1,34 +1,35 @@
 import chat_meta
 from langchain.messages import AIMessage, HumanMessage
 from stm import ShortTermMemory, MessageContext
-from logger import logger
+from logger import log_conversation
 
-def main(agent, chat_meta: chat_meta.ChatMeta, user_prompt: str = ""):
+def main(agent, chat_meta: chat_meta.ChatMeta):
     stm = ShortTermMemory(message_contexts=MessageContext(messages=[]), token_count=0)
-    # bug here, i suspect it does not do the cli stuff because of the generator
-    if user_prompt == "":
-        while True:
-            user_prompt = input(f"{chat_meta.username}: ")
-            stm.add_message(HumanMessage(content=user_prompt))
-
-            print(f"{chat_meta.model_name}: ", end="", flush=True)
-            response = ""
-            for token in agent.stream({"messages": stm.get_comprehensive_context()}, stream_mode="messages"): # type: ignore
-                if token[0].type == "AIMessageChunk": # type: ignore
-                    print(token[0].content, end="", flush=True) # type: ignore
-                    response += token[0].content  # type: ignore
-            
-            print("\n\n")
-            stm.add_message(AIMessage(content=response))
-            logger(chat_meta.keep_logs, stm.get_message_contexts().model_dump(), filename="agentic-sum.log")
-    else:
+    while True:
+        user_prompt = input(f"({chat_meta.username}): ")
         stm.add_message(HumanMessage(content=user_prompt))
 
+        print(f"({chat_meta.model_name}): ", end="", flush=True)
         response = ""
         for token in agent.stream({"messages": stm.get_comprehensive_context()}, stream_mode="messages"): # type: ignore
             if token[0].type == "AIMessageChunk": # type: ignore
+                print(token[0].content, end="", flush=True) # type: ignore
                 response += token[0].content  # type: ignore
-                yield token[0].content  # type: ignore
-        
+
+        print("\n\n")
         stm.add_message(AIMessage(content=response))
-        logger(chat_meta.keep_logs, stm.get_message_contexts().model_dump(), filename="agentic-ui.log")
+        log_conversation(chat_meta.keep_logs, stm.get_message_contexts().model_dump(), filename="agentic-sum.log")
+
+def stream(agent, chat_meta: chat_meta.ChatMeta, user_prompt: str, stm: ShortTermMemory | None = None):
+    if stm is None:
+        stm = ShortTermMemory(message_contexts=MessageContext(messages=[]), token_count=0)
+    stm.add_message(HumanMessage(content=user_prompt))
+
+    response = ""
+    for token in agent.stream({"messages": stm.get_comprehensive_context()}, stream_mode="messages"): # type: ignore
+        if token[0].type == "AIMessageChunk": # type: ignore
+            response += token[0].content  # type: ignore
+            yield token[0].content  # type: ignore
+
+    stm.add_message(AIMessage(content=response))
+    log_conversation(chat_meta.keep_logs, stm.get_message_contexts().model_dump(), filename="agentic-ui.log")
